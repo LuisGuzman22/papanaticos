a9e7c804a40397b77615b14c566e03f718aab739
Luis Guzman <luis.guzmanpena@externos-cl.cencosud.com>
2021-12-17 21:52:47 -0300

    primero

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import "./App.css";
-import logo from "./logo.svg";
+import { HomePage } from "./components/pages/homePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
 }
