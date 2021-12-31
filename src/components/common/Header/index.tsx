3a6217da162519e2f3bbbab0098da0fedf06ebb4
Luis Guzman <luis.guzmanpena@externos-cl.cencosud.com>
2021-12-31 20:27:39 -0300

    separacion modal

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -10,12 +10,12 @@ import {
   Form,
   Row,
 } from "react-bootstrap";
+import { ModalRegister } from "../ModalRegister";
 
 export const HeaderComponent = () => {
   const [show, setShow] = useState(false);
 
   const handleModal = () => {
-    console.log("handle");
     setShow(true);
   };
 
@@ -46,82 +46,7 @@ export const HeaderComponent = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Registro de pedido</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label column sm="2">
-                Teléfono
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control defaultValue="+569 5116 7340" />
-              </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label column sm="2">
-                Pedido
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control placeholder="Papitas" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label column sm="2">
-                Tamaño
-              </Form.Label>
-              <Col sm="10">
-                <Form.Select aria-label="XL">
-                  <option>Tamaño</option>
-                  <option value="1">Normal</option>
-                  <option value="2">XL</option>
-                </Form.Select>
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label column sm="2">
-                Cliente
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control placeholder="Luis Guzmán" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label column sm="2">
-                Dirección
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control placeholder="Manuel Bayon 639 casa 54" />
-              </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label column sm="2">
-                Comentario
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control
-                  placeholder="bla bla bla"
-                  as="textarea"
-                  rows={3}
-                />
-              </Col>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cerrar
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Registrar pedido
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <ModalRegister handleClose={handleClose} show={show} />
     </div>
   );
 };
---
+++ src/components/common/ModalRegister/index.tsx
@@ -0,0 +1,90 @@
+import React, { MouseEventHandler } from "react";
+import { Modal, Form, Row, Col, Button } from "react-bootstrap";
+
+interface IProps {
+  show: boolean;
+  handleClose: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const ModalRegister = (props: IProps) => {
+  const { show, handleClose } = props;
+  return (
+    <div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Registro de pedido</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                Teléfono
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control defaultValue="+569 5116 7340" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                Pedido
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control placeholder="Papitas" />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                Tamaño
+              </Form.Label>
+              <Col sm="10">
+                <Form.Select aria-label="XL">
+                  <option>Tamaño</option>
+                  <option value="1">Normal</option>
+                  <option value="2">XL</option>
+                </Form.Select>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                Cliente
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control placeholder="Luis Guzmán" />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                Dirección
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control placeholder="Manuel Bayon 639 casa 54" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="2">
+                Comentario
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  placeholder="bla bla bla"
+                  as="textarea"
+                  rows={3}
+                />
+              </Col>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Registrar pedido
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
