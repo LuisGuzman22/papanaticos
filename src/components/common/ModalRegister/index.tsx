ffaed68dd5e65fe609ba51de43c9c460f58a0ef5
Luis Guzman <luis.guzmanpena@externos-cl.cencosud.com>
2022-01-04 13:09:10 -0300

    WIP diseño

--- src/components/common/ModalRegister/index.tsx
+++ src/components/common/ModalRegister/index.tsx
@@ -21,7 +21,9 @@ export const ModalRegister = (props: IProps) => {
   const [clientName, setclientName] = useState("");
   const [address, setaddress] = useState("");
   const [comment, setcomment] = useState("");
-
+  const [disableUsesr, setdisableUsesr] = useState(true);
+  const [disableAddress, setdisableAddress] = useState(true);
+  const [createUser, setcreateUser] = useState(false);
   const [clientData, setclientData] = useState<IClient>();
 
   const handleRegister = (event: any) => {
@@ -38,11 +40,28 @@ export const ModalRegister = (props: IProps) => {
         created_at: firebase.firestore.FieldValue.serverTimestamp(),
       })
       .then((res) => {
-        console.log("res", res);
+        console.log("orden registrada", res);
       })
       .catch((e) => {
         console.log("e", e);
       });
+
+    if (createUser) {
+      firestore
+        .collection("clients")
+        .add({
+          address,
+          name: clientName,
+          phone: phoneNumber,
+          created_at: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+        .then((res) => {
+          console.log("res", res);
+        })
+        .catch((e) => {
+          console.log("e", e);
+        });
+    }
     handleClose(event);
   };
 
@@ -51,24 +70,41 @@ export const ModalRegister = (props: IProps) => {
   };
 
   useEffect(() => {
-    console.log("phone", phoneNumber);
-    firestore
-      .collection("clients")
-      .where("phone", "==", phoneNumber)
-      .onSnapshot((snapshot) => {
-        snapshot.forEach((element) => {
-          const data: IClient = {
-            address: element.data().address,
-            name: element.data().name,
-          };
-          setclientData(data);
+    if (phoneNumber.length === 9) {
+      firestore
+        .collection("clients")
+        .where("phone", "==", phoneNumber)
+        .onSnapshot((snapshot) => {
+          snapshot.forEach((element) => {
+            const data: IClient = {
+              address: element.data().address,
+              name: element.data().name,
+            };
+            setclientData(data);
+          });
         });
-      });
+      setdisableAddress(false);
+      setdisableUsesr(false);
+    } else {
+      setdisableAddress(true);
+      setdisableUsesr(true);
+      setclientName("");
+      setaddress("");
+    }
   }, [phoneNumber]);
 
   useEffect(() => {
-    console.log("clientData", clientData);
+    // console.log("clientData", clientData);
+    if (!clientData) {
+      setcreateUser(true);
+    } else {
+      setcreateUser(false);
+    }
   }, [clientData]);
+
+  // useEffect(() => {
+  //   console.log("createUser", createUser);
+  // }, [createUser]);
 
   const handleChangeOrder = (event: any) => {
     setorder(event.target.value);
@@ -141,8 +177,14 @@ export const ModalRegister = (props: IProps) => {
                 <Form.Control
                   placeholder="Luis Guzmán"
                   onChange={handleChangeClient}
-                  value={clientData?.name ? clientData?.name : ""}
-                  disabled={clientData?.name ? false : true}
+                  value={
+                    clientName
+                      ? clientName
+                      : clientData?.name
+                      ? clientData?.name
+                      : ""
+                  }
+                  disabled={disableUsesr}
                 />
               </Col>
             </Form.Group>
@@ -154,8 +196,14 @@ export const ModalRegister = (props: IProps) => {
                 <Form.Control
                   placeholder="Manuel Bayon 639 casa 54"
                   onChange={handleChangeAddress}
-                  value={clientData?.address ? clientData?.address : ""}
-                  disabled={clientData?.address ? false : true}
+                  value={
+                    address
+                      ? address
+                      : clientData?.address
+                      ? clientData?.address
+                      : ""
+                  }
+                  disabled={disableAddress}
                 />
               </Col>
             </Form.Group>
