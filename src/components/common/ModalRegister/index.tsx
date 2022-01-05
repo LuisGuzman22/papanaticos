056db60c9fcae204ee3eb9a522f3346e1ba90df4
Luis Guzman <luis.guzmanpena@externos-cl.cencosud.com>
2022-01-05 10:30:52 -0300

    test

--- src/components/common/ModalRegister/index.tsx
+++ src/components/common/ModalRegister/index.tsx
@@ -1,13 +1,28 @@
-import React, { MouseEventHandler } from "react";
-import { Modal, Form, Row, Col, Button } from "react-bootstrap";
+import React, { MouseEventHandler, useState } from "react";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
 
 interface IProps {
   show: boolean;
   handleClose: MouseEventHandler<HTMLButtonElement>;
 }
 
+interface IClient {
+  name: string;
+  address: string;
+}
+
 export const ModalRegister = (props: IProps) => {
   const { show, handleClose } = props;
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [order, setorder] = useState("");
+  const [size, setsize] = useState("");
+  const [clientName, setclientName] = useState("");
+  const [address, setaddress] = useState("");
+  const [comment, setcomment] = useState("");
+  const [disableUsesr, setdisableUsesr] = useState(true);
+  const [disableAddress, setdisableAddress] = useState(true);
+  const [createUser, setcreateUser] = useState(false);
+  const [clientData, setclientData] = useState<IClient>();
 
   const handleRegister = (event: any) => {
     console.log(event.target);
@@ -15,20 +30,54 @@ export const ModalRegister = (props: IProps) => {
     event.preventDefault();
   };
 
+  const handleChangeOrder = (event: any) => {
+    setorder(event.target.value);
+  };
+
+  const handleChangeSize = (event: any) => {
+    setsize(event.target.value);
+  };
+
+  const handleChangeClient = (event: any) => {
+    setclientName(event.target.value);
+  };
+
+  const handleChangeAddress = (event: any) => {
+    setaddress(event.target.value);
+  };
+
+  const handleChangeComment = (event: any) => {
+    setcomment(event.target.value);
+  };
+
+  const handleChangePhone = (event: any) => {
+    setphoneNumber(event.target.value);
+  };
+
   return (
     <div>
-      <Modal show={show}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Registro de pedido</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
             <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="3"></Form.Label>
+              <Col sm="9">
+                <Button variant="outline-dark">Retiro en tienda</Button>
+                <Button variant="outline-dark">Delivery</Button>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
               <Form.Label column sm="2">
                 Teléfono
               </Form.Label>
               <Col sm="10">
-                <Form.Control defaultValue="+569 5116 7340" />
+                <Form.Control
+                  placeholder="Teléfono"
+                  onChange={handleChangePhone}
+                />
               </Col>
             </Form.Group>
 
@@ -37,7 +86,10 @@ export const ModalRegister = (props: IProps) => {
                 Pedido
               </Form.Label>
               <Col sm="10">
-                <Form.Control placeholder="Papitas" />
+                <Form.Control
+                  placeholder="Pedido"
+                  onChange={handleChangeOrder}
+                />
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3">
@@ -45,10 +97,10 @@ export const ModalRegister = (props: IProps) => {
                 Tamaño
               </Form.Label>
               <Col sm="10">
-                <Form.Select aria-label="XL">
+                <Form.Select aria-label="XL" onChange={handleChangeSize}>
                   <option>Tamaño</option>
-                  <option value="1">Normal</option>
-                  <option value="2">XL</option>
+                  <option value="Normal">Normal</option>
+                  <option value="XL">XL</option>
                 </Form.Select>
               </Col>
             </Form.Group>
@@ -57,7 +109,18 @@ export const ModalRegister = (props: IProps) => {
                 Cliente
               </Form.Label>
               <Col sm="10">
-                <Form.Control placeholder="Luis Guzmán" />
+                <Form.Control
+                  placeholder="Cliente"
+                  onChange={handleChangeClient}
+                  value={
+                    clientName
+                      ? clientName
+                      : clientData?.name
+                      ? clientData?.name
+                      : ""
+                  }
+                  disabled={disableUsesr}
+                />
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3">
@@ -65,7 +128,18 @@ export const ModalRegister = (props: IProps) => {
                 Dirección
               </Form.Label>
               <Col sm="10">
-                <Form.Control placeholder="Manuel Bayon 639 casa 54" />
+                <Form.Control
+                  placeholder="Dirección"
+                  onChange={handleChangeAddress}
+                  value={
+                    address
+                      ? address
+                      : clientData?.address
+                      ? clientData?.address
+                      : ""
+                  }
+                  disabled={disableAddress}
+                />
               </Col>
             </Form.Group>
 
@@ -78,6 +152,7 @@ export const ModalRegister = (props: IProps) => {
                   placeholder="bla bla bla"
                   as="textarea"
                   rows={3}
+                  onChange={handleChangeComment}
                 />
               </Col>
             </Form.Group>
